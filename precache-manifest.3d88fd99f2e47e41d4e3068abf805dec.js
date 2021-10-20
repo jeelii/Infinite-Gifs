99bc465e658607a44bc85c73682a58df960886c7
Jenny <jenny@youwza.com>
2021-10-20 12:28:16 +0200

    Updates

---
+++ precache-manifest.3d88fd99f2e47e41d4e3068abf805dec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd8b4c1b60270f6e61ca183ebf918136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f0ef36278c6000609d83",
+    "url": "/static/css/main.c6b79044.chunk.css"
+  },
+  {
+    "revision": "bfc54ba952db69711bc5",
+    "url": "/static/js/2.ba82f77d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.ba82f77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0ef36278c6000609d83",
+    "url": "/static/js/main.e4cd8afd.chunk.js"
+  },
+  {
+    "revision": "aaae2fe2200a2e500a17",
+    "url": "/static/js/runtime-main.9e32fd90.js"
+  }
+]);
